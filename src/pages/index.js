68dcdc3569858e739a18919683a5060184794d02
Thomas Roman <thomasroman32@yahoo.com>
2019-11-06 23:57:14 -0600

    changes to the example blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>This is a sample site for the Gatsby Crash Course</p>
+    <h1>Hello</h1>
+    <p>This is my blog</p>
     
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
