27a4ef907a05a68d3a248a07453b1f1802f74ba1
Thomas Roman <thomasroman32@yahoo.com>
2019-11-25 23:59:03 -0600

    removing the gatsby link in the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -40,7 +40,7 @@ const Layout = ({ children }) => (
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            {/* <a href="https://www.gatsbyjs.org">Gatsby</a> */}
           </footer>
         </div>
       </>
