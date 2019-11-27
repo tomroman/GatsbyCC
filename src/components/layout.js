f4d622b1b0d85f8823d3a769c72bc8a25e621d9d
Thomas Roman <thomasroman32@yahoo.com>
2019-11-26 23:30:33 -0600

    removed the Built with in the footer.

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,7 +38,7 @@ const Layout = ({ children }) => (
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
+            © {new Date().getFullYear()}
             {` `}
             {/* <a href="https://www.gatsbyjs.org">Gatsby</a> */}
           </footer>
